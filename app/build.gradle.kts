69e406e524b01334173375eb40ab79f2fbcaad5e
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-25 09:25:00 +0100

    Upgrade target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "dev.dlovan.androidblackjack"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dev.dlovan.androidblackjack"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
